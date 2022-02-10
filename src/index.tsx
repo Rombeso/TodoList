8c89e8970619adeca388ec7224bdf0607bb25301
Rombeso <larionovra@gmail.com>
2022-02-10 18:27:47 +0300

    The new build for 14 lesson.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import App from './App';
-import {Provider} from 'react-redux';
-import {store} from './state/store';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './state/store';
 
 ReactDOM.render(
     <React.StrictMode>
